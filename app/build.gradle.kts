2dbc9665255de38f50f73b7f65b595d8c5b18b34
krossmanzs <radi.adi78@gmail.com>
2022-03-02 22:36:47 +0700

    Ganti JUnit 5 dan menambahkan dependency coroutine, serta ganti versi kotlin ke 1.6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -29,11 +29,16 @@ dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Junit 5
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+}
+
+// Mengganti default unit test menjadi JUnit 5
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
 
 application {
