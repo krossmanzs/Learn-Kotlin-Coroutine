d972c63a058dbecedfe29b15614236953e42b03d
krossmanzs <radi.adi78@gmail.com>
2022-03-02 22:55:20 +0700

    Membuat Thread

--- app/src/test/kotlin/krossmanzs/coroutine/ThreadTest.kt
+++ app/src/test/kotlin/krossmanzs/coroutine/ThreadTest.kt
@@ -1,11 +1,37 @@
 package krossmanzs.coroutine
 
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.concurrent.thread
 
 class ThreadTest {
     @Test
     internal fun testThreadName() {
         val threadName = Thread.currentThread().name
         println("Running in thread $threadName")
+    }
+
+    @Test
+    fun testNewThread() {
+        // BAWAAN JAVA
+//        val runnable = Runnable {
+//            println(Date())
+//            Thread.sleep(2_000)
+//            println("Finish : ${Date()}")
+//        }
+//
+//        val thread = Thread(runnable)
+//        thread.start()
+
+        // KOTLIN VERSION :) lebih simple
+        thread(start = true) {
+            println(Date())
+            Thread.sleep(2_000)
+            println("Finish : ${Date()}")
+        }
+
+        println("WAIT FOR thread TO FINISH")
+        Thread.sleep(3_000)
+        println("END FUNCTION")
     }
 }
