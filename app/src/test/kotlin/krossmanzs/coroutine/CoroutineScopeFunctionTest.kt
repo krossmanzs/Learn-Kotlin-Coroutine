58473a240772389788cc43f6d8da7717bf25b307
krossmanzs <radi.adi78@gmail.com>
2022-05-07 16:04:06 +0700

    Coroutine Scope Parent & Child

--- app/src/test/kotlin/krossmanzs/coroutine/CoroutineScopeFunctionTest.kt
+++ app/src/test/kotlin/krossmanzs/coroutine/CoroutineScopeFunctionTest.kt
@@ -2,6 +2,7 @@ package krossmanzs.coroutine
 
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
 
 /**
  * CoroutineScope Function
@@ -42,6 +43,38 @@ class CoroutineScopeFunctionTest {
 
         runBlocking {
             job.join()
+        }
+    }
+
+    /**
+     * Coroutine Scope Parent & Child
+     *
+     * Saat membuat coroutine scope, sebenarnya kita telah
+     * membuat child scope dari parent scopenya
+     *
+     * Saat kita membuat child scope, secara otomatis child scope
+     * akan menggunakan dispatcher milik parent dan saat kita
+     * membatalkan parent scope, maka semua child scopenya pun akan
+     * dibatalkan
+     */
+
+    @Test
+    fun testChildDispatcher() {
+        val parentDispatcher = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+        val parentScope = CoroutineScope(parentDispatcher)
+
+        val job = parentScope.launch {
+            println("Parent Scope run in ${Thread.currentThread().name}")
+            coroutineScope {
+                launch {
+                    delay(2_000)
+                    println("Child Scope run in ${Thread.currentThread().name}")
+                }
+            }
+        }
+
+        runBlocking {
+            job.cancelAndJoin()
         }
     }
 }
