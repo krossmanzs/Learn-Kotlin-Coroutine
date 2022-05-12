da7816bff828134cebeff6e1b89d2d9a8cc3d230
krossmanzs <radi.adi78@gmail.com>
2022-05-13 01:27:49 +0700

    Semaphore

--- app/src/test/kotlin/krossmanzs/coroutine/LockingTest.kt
+++ app/src/test/kotlin/krossmanzs/coroutine/LockingTest.kt
@@ -2,7 +2,9 @@ package krossmanzs.coroutine
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.sync.withPermit
 import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 
@@ -59,6 +61,40 @@ class LockingTest {
         runBlocking {
             delay(5_000)
             println("Total counter : $counter")
+        }
+    }
+
+    /**
+     * Semaphore
+     * sama seperti Mutex, Semaphore juga digunakan sebagai object
+     * untuk locking.
+     *
+     * Namun yang membedakan, pada Mutex, kita hanya memperbolehkan 1
+     * coroutine yang bisa mengakses nya pada satu waktu
+     *
+     * Namun pada Semaphore, kita bisa menentukan berapa jumlah coroutine
+     * yang boleh mengakses nya pada satu waktu.
+     */
+    @Test
+    fun testSemaphore() {
+        var counter = 0
+        val dispatcher = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+        val scope = CoroutineScope(dispatcher)
+        val semaphore = Semaphore(permits = 2)
+
+        repeat(100) {
+            scope.launch {
+                repeat(1000){
+                    semaphore.withPermit {
+                        counter++
+                    }
+                }
+            }
+        }
+
+        runBlocking {
+            delay(5_000)
+            println("Total counter: $counter")
         }
     }
 }
