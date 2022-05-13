fbef432d3651d0b67b1259700d0b6380d0234b9b
krossmanzs <radi.adi78@gmail.com>
2022-05-13 16:13:33 +0700

    Flow Exception

--- app/src/test/kotlin/krossmanzs/coroutine/FlowTest.kt
+++ app/src/test/kotlin/krossmanzs/coroutine/FlowTest.kt
@@ -76,4 +76,35 @@ class FlowTest {
                 .collect { println(it) }
         }
     }
+
+    /**
+     * Flow Exception
+     *
+     * Saat terjadi exception pada flow, di bagian operator
+     * apapun, maka flow akan berhenti, lalu exception akan
+     * di throw oleh flow.
+     *
+     * Untuk menangkap exception tersebut, kita bisa menggunakan
+     * block try-catch.
+     *
+     * Namun flow juga menyediakan operator untuk menangkap exception
+     * tersebut, nama functionnya adalah catch()
+     *
+     * Untuk finally, flow menggunakan onCompletion()
+     *
+     * Jika terjadi error di flow, flow akan dihentikan, jika kita ingin
+     * flow tidak berhenti saat terjadi error, pastikan kita selalu
+     * melakukan try catch di kode flow nya.
+     */
+    @Test
+    fun testFlowException() {
+        runBlocking {
+            val flow = numberFlow()
+            flow.map { check(it < 10); it }
+                .onEach { println(it) }
+                .catch { println("Error ${it.message}") }
+                .onCompletion { println("Done || Finally") }
+                .collect()
+        }
+    }
 }
