73479c909db5b3e5b7d2a9ee8b930f68b7d34111
krossmanzs <radi.adi78@gmail.com>
2022-05-13 20:20:48 +0700

    State Flow

--- app/src/test/kotlin/krossmanzs/coroutine/FlowTest.kt
+++ app/src/test/kotlin/krossmanzs/coroutine/FlowTest.kt
@@ -129,4 +129,50 @@ class FlowTest {
             }.join()
         }
     }
+
+    /**
+     * State Flow
+     * adalah turunan dari Shared Flow, artinya di State Flow,
+     * kita bisa membuat banyak receiver
+     *
+     * Pada State Flow, receiver hanya akan menerima data paling
+     * baru. Jadi jika ada receiver yang sangat lambat dan sender
+     * mengirim data terlalu cepat, yang akan diterima oleh receiver
+     * adalah data paling akhir.
+     *
+     * State Flow cocok digunakan untuk maintain state, dimana memang
+     * biasanya state itu biasanya hanya satu data, tidak peduli beberapa
+     * kali perubahan data tersebut, yang paling penting pada state
+     * adalah data terakhir.
+     *
+     * Untuk mendapatkan data statenya, kita bisa menggunakan field value
+     * di state Flow.
+     *
+     * Untuk membuat receivernya kita bisa menggunakan asStateFlow()
+     *
+     * State Flow bisa dirancang sebagai pengganti Conflated Broadcast Channel
+     */
+    @Test
+    fun testStateFlow() {
+        val stateFlow = MutableStateFlow(value = 0)
+        val scope = CoroutineScope(Dispatchers.IO)
+        runBlocking {
+            scope.launch {
+                repeat(10) {
+                    delay(100)
+                    println("Emit $it")
+                    stateFlow.emit(it)
+                }
+            }
+            scope.launch {
+                val stateFlow1 = stateFlow.asStateFlow()
+                stateFlow1.collect() {
+                    delay(250)
+                    println("State Flow 1 receive $it")
+                }
+            }
+            delay(5_000)
+            scope.cancel()
+        }
+    }
 }
